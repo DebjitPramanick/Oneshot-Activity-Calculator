305e09b8ee3ae636e4be6188129258c378dec634
DebjitPramanick <dpramanick9990@gmail.com>
2023-03-30 20:10:18 +0530

    Fix: Pass val #1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from 'react'
 import Calculator from '../components/Calculator'
 import { HomeLayout } from '../styles/Layout'
 
 const Home = () => {
+
+    useEffect(() => {
+        window.addEventListener('message', function (event) {
+            var origin = event.origin; // For Chrome, the origin property is in the event.originalEvent object.
+            console.log("ORIGIN", origin)
+            if (origin !== 'https://ld15l5.csb.app/')
+                return;
+            if (typeof event.data == 'object' && event.data.call == 'sendValue') {
+                console.log("DATA", event.data.value)
+                // Do something with event.data.value;
+            }
+        }, false);
+    }, [])
+
     return (
         <HomeLayout>
             <Calculator />
