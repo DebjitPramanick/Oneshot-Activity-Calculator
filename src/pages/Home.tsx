9d665173c1a13f3099b524e68da8c18900c5011d
DebjitPramanick <dpramanick9990@gmail.com>
2023-03-30 20:14:15 +0530

    Fix: Revert

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,9 @@
-import { useEffect, useState } from 'react'
 import Calculator from '../components/Calculator'
 import { HomeLayout } from '../styles/Layout'
 
 const Home = () => {
-
-    const [data, setdata] = useState('')
-
-    useEffect(() => {
-        window.addEventListener('message', function (event) {
-            var origin = event.origin; // For Chrome, the origin property is in the event.originalEvent object.
-            console.log("ORIGIN", origin)
-            if (origin !== 'https://ld15l5.csb.app/')
-                return;
-            if (typeof event.data === 'object' && event.data.call === 'sendValue') {
-                console.log("DATA", event.data.value)
-                setdata(JSON.stringify(event.data.value))
-                // Do something with event.data.value;
-            }
-        }, false);
-    }, [])
-
     return (
         <HomeLayout>
-            <h1>Here is the data {data}</h1>
             <Calculator />
         </HomeLayout>
     )
