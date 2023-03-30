d456b8e119f4f69b46603beaadbd2fd49f8fd6a6
DebjitPramanick <dpramanick9990@gmail.com>
2023-03-30 21:37:42 +0530

    Fix: WIP Labels

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,10 @@
-import { useEffect, useState } from 'react'
 import Calculator from '../components/Calculator'
 import { HomeLayout } from '../styles/Layout'
 
 const Home = () => {
 
-    const [data, setdata] = useState('')
-
-    useEffect(() => {
-        const getIframeData = (e: any) => {
-            if (e.origin !== 'http://localhost:3001') return;
-            setdata(JSON.stringify(e.data))
-        }
-        window.addEventListener('message', getIframeData)
-
-        return () => {
-            window.removeEventListener('message', getIframeData)
-        }
-    }, [])
-
     return (
         <HomeLayout>
-            <h1>Here is the data: {data}</h1>
             <Calculator />
         </HomeLayout>
     )
