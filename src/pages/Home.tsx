09d56370dbb793a9aed96a4b1db3c7dcebd53d2b
DebjitPramanick <dpramanick9990@gmail.com>
2023-03-30 20:18:33 +0530

    Test: Show data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react'
 import Calculator from '../components/Calculator'
 import { HomeLayout } from '../styles/Layout'
 
 const Home = () => {
+
+    const [data, setdata] = useState('')
+
+    useEffect(() => {
+        window.addEventListener('message', function (event) {
+            var origin = event.origin; // For Chrome, the origin property is in the event.originalEvent object.
+            console.log("ORIGIN", origin)
+            if (origin !== 'https://oneshot-activity-calculator-git-dev-debjitpramanick.vercel.app/')
+                return;
+            if (typeof event.data === 'object' && event.data.call === 'sendValue') {
+                console.log("DATA", event.data.value)
+                setdata(JSON.stringify(event.data.value) + origin)
+                // Do something with event.data.value;
+            }
+        }, false);
+    }, [])
+
     return (
         <HomeLayout>
+            <h1>Here is the data {data}</h1>
             <Calculator />
         </HomeLayout>
     )
